1796066770b1eeeeed0b5b6d4fddc0b3f2004cea
sir-mills <etiosakings@gmail.com>
2025-04-08 07:21:50 +0100

    fix:jwt auth

--- nest-js_task/src/user/user.moudle.ts
+++ nest-js_task/src/user/user.moudle.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  imports: [JwtModule],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [UserService, UserResolver, PrismaService],
   exports: [UserService],
 })
